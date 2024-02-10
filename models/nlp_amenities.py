22bd674fa60edae67ab0f46612f4a02b6191bf32
jayeun <jackyeung99@gmail.com>
2024-02-10 15:23:37 -0500

    linked nlp processing into preprocessing

--- models/nlp_amenities.py
+++ models/nlp_amenities.py
@@ -7,13 +7,24 @@ from nltk.corpus import stopwords
 from gensim.models import KeyedVectors
 from gensim.models.phrases import Phrases, Phraser
 from scipy.spatial.distance import cosine
+import os
+
+
+# download once
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 
 class nlp_processor:
-    def __init__(self, word2vec_model,input_list):
-        self.input_list = input_list
-        self.lemmatizer = WordNetLemmatizer()
-        self.word2vec_model = word2vec_model
-        self.categories = {
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(nlp_processor, cls).__new__(cls)
+            model_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'GoogleNews-vectors-negative300.bin.gz')
+            cls._instance.word2vec_model = KeyedVectors.load_word2vec_format(model_path, binary=True, limit=100000)
+            cls._instance.lemmatizer = WordNetLemmatizer()
+            cls._instance.categories = {
             'Leisure': ['pool', 'grill', 'courtyard', 'clubhouse', 'lounge', 'spa','rooftop'],
             'Technology': ['wi-fi', 'high speed internet access', 'electric car charging', 'wifi'],
             'Services': ['mail room','laundry facilities', 'maintenance', 'package service', 'valet trash removal','cleaning','parking garage'],
@@ -23,14 +34,14 @@ class nlp_processor:
             'Apartment_features': ['kitchen island','granite countertops','12ft ceilings','Walk-in closet','cabinetry','desk','shower'],
             'Appliances': ['dishwasher','stainless_steel appliances','washer','dryer','refrigerator','sink','microwave']
         }
+        return cls._instance
 
     def preprocess_text(self, text):
         tokens = word_tokenize(text.lower())
-        lemmatized = [self.lemmatizer.lemmatize(token) for token in tokens if token not in stopwords.words('english') and token.isalpha()]
-        return lemmatized
+        return [self.lemmatizer.lemmatize(token) for token in tokens if token.isalpha() and token not in stopwords.words('english')]
 
-    def phrase_detection(self):
-        tokenized_list = [self.preprocess_text(item) for item in self.input_list]
+    def phrase_detection(self,input_list):
+        tokenized_list = [self.preprocess_text(item) for item in input_list]
         phrases = Phrases(tokenized_list, min_count=1, threshold=1)
         bigram = Phraser(phrases)
         return [' '.join(bigram[tokens]) for tokens in tokenized_list]
@@ -40,8 +51,8 @@ class nlp_processor:
         vectors = [self.word2vec_model[token] for token in tokens if token in self.word2vec_model]
         return np.mean(vectors, axis=0) if vectors else None
 
-    def categorize_items(self):
-        phrased_list = self.phrase_detection()
+    def categorize_items(self,input_list):
+        phrased_list = self.phrase_detection(input_list)
         categorized_items = {category: [] for category in self.categories}
         categorized_items['Others'] = []
 
@@ -69,29 +80,31 @@ class nlp_processor:
         return categorized_items
     
 
-    def output_vector(self):
-        categorized_items = self.categorize_items()
+    def output_vector(self,categories):
+       
         base_vector = np.zeros(len(self.categories))
-        category_len = {key: len(items) for key,items in categorized_items.items() if key != "Others"}
-        items = np.array(category_len.values())
+        category_len = {key: len(items) for key,items in categories.items() if key != "Others"}
 
         for i, category in enumerate(sorted(self.categories)):
             if category != "Others":
                 # Update the base vector with the count of items in the current category
                 base_vector[i] = category_len.get(category, 0)
         
-        print(base_vector)
         return base_vector
+
+    def convert_amenities_to_vector(self,input_list):   
+        categorized_items = self.categorize_items(input_list)
+        vector = self.output_vector(categorized_items)
+        return vector
 
     def main(self):
         categorized_items = self.categorize_items()
+        vector = self.output_vector()
         for category, items in categorized_items.items():
             print(f"{category}: {items}")
-        self.output_vector()
+        print(vector)
 
 if __name__ == "__main__":
-    model_path = '../../GoogleNews-vectors-negative300.bin.gz'
-    word2vec_model = KeyedVectors.load_word2vec_format(model_path, binary=True, limit=100000)
     test_list = [ 'double vanities ‘', 'entertaining clubhouse w/ kitchen', 'guest apartment', 'stackable washer & dryer rental', 'on-site storage', 'on site laundry', 'gourmet island kitchen', 'waterfront', 'built-in desk/shelving', 'pet-friendly apartments', 'hardwood styled flooring in select apartments', 'linen closets with built-in shelving', 'luxury vinyl plank flooring in sea coast', 'custom tile', 'spin room', 'wood style plank flooring', 'weekly happy hours', 'microwave in select units', 'nightpatrol', 'picturesque hill country views', 'full size pantries', 'european washer and dryers', 'garden bathtub in select homes', 'landscaped garden', 'wood-style plank flooring in living areas', 'on-site park', '60% amfi unit', 'granite counters in kitchen and bath', 'w/d included', 'the coffee shop', 'porch', 'health-focused air-filtration', 'cabanas', 'wood-style plank flooring throughout (select 1st f', 'handicap accessible', 'upgraded lighting fixtures', 'garden windows in selected units', 'maintenance on-site', 'shower / tub combo', 'custom-framed mirrors with specialty lighting', 'pet wash', '6 inch baseboards', 'day care center on-site', 'oversized rooms & closets', 'large windows with natural lighting', 'full-size top load washer and dryer', 'custom color accent walls available', 'skyline terrace with downtown views', 'dry cleaning services on-site', 'detached garages available', 'ceiling fan', '24 hour mw dry cleaning service', 'ut austin campus nearby', 'ground level', 'ceiling fans in living rooms & bedrooms', 'private screening room with stadium seating', 'no breed or weight restrictions', 'subway tile backsplash in kitchen', 'pet friendly with bark park', 'floor 13', 'indoor mail center', 'island kitchens with granite countertops', '2-inch faux wood window blinds', 'two designer finish packages with accent wall in living room', 'decorative subway tile backsplash', 'wood-style flooring and oversized windows offering sweeping views of austin', 'coworking spaces', 'cozy fireplaces', 'poolside grills & picnic area', 'community room', 'svc cable', 'brushed nickel light fixtures & hardware', 'monthly resident events', 'beautiful landscaped grounds with picnic areas', 'package room', 'package service', 'exclusive 5th street resident lounge', 'convenient location', 'open air fitness courtyard', 'tile floors', 'subway tile backsplash', 'built-in desks & bookcases', 'yards attached to select homes', 'first floor', 'sport court', 'rainfall shower heads', 'outdoor poolside kitchen', 'energy star front-load washer & dryer', '24-hour resident clubhouse access', 'poolside lounge with hdtvs and a fireplace', 'uniquely designed', 'pet wash station', 'access controlled entries and elevators', 'hardwood-style flooring', 'coworking space', 'ev charging stations', 'immediate access to downtown austin’s unique dining, trendy shopping and lively entertainment option', 'door-to-door trash pick-up & trash chutes', 'oversized chess board and social games', 'gas range', 'central location with freeway access', 'espresso cabinetry with stainless-steel pulls', 'abundant natural light throughout', 'open concept residences', 'covered poolside veranda', 'wine fridge in unit', 'austin energy green building', 'refrigerators with ice-makers', 'pet friendly', 'alarm ready', 'bbq grill/food prep area', 'xl patio/balcony', 'easy access to mass transit', 'garden tubs', 'balcony in select units', 'separate dining area', 'private patio or balcony', 'touchless water refill stations', 'large heated plunge spa', '7-level private parking garage', 'clubhouse lounge', 'recycling pick-up at your front door', 'minutes to downtown austin & austin nightlife', 'oval garden tubs', 'quartz countertops (deluxe homes)', 'upgraded 2- floorplans', 'bar & lounge', 'standard patio (38-54sq ft)', 'intrusion alarm systems', 'brushed nickel lighting', 'study rooms', 'double basin undermount sink with sprayer faucet', '4 electric car charging stations', 'secured parking garage', '3.0 miles to lady bird lake', 'resident lounge featuring seating and flat screens with attached kitchen', 'energy star appliances with french door refrigerators', '18-20 foot ceilings in select units', 'easy access to freeways, dining and shopping', 'spectrum wifi & cable', 'pet friendly, amazing staff', '24-hour to-go marketplace for food, beverage and convenience items', 'gorgeous lush landscaping', 'energy star® stainless steel appliance packages', 'single basin sink w/ gooseneck pull down faucet', 'gated bark park', 'dual-access closet', 'single basin kitchen sink', 'built in bookshelves ', 'grilling stations', 'resident portal- pay rent/request maint.', 'full size front loader washer and dryer', 'bike storage room', 'relaxing resort style pool', 'close to shopping', 'washer and dryer', 'one & two bedroom apartments & carriage homes', 'leed gold certified building', '2 resort-style pools and sundeck', 'smart unit', 'ten foot exposed concrete ceilings', 'entertainment room', 'no neighbors up or downstairs.', 'balcony, deck or patio', 'pool deck seating', '24 hour dry cleaning service', 'smart-home integration', 'easy access to freeways and shopping', 'three refreshing swimming pools', 'custom cabinetry with brushed nickel hardware', 'spanish speaking staff', 'private dining at cork', 'roof top', 'complimentary bikes available', '4th floor', 'energy-efficient stainless steel appliances', '42" hardwood kitchen cabinets', 'fenced backyards', 'walk-in showers with built-in bench', 'spa', 'custom tile backsplashes', 'farmhouse-style kitchen sinks', 'washer/dryer hookup', 'designer pendant lighting', 'octagon functional training', 'double vanity', 'dry bar, terrace, computer desk', 'walk in closets', 'washer/dryer in residence', 'faux hardwood or polished concrete flooring', 'bocce lawn', 'double vanity sinks ', 'high ceilings', 'energy star certified building', 'new/renovated interior', 'limited access gates', 'double vanity ', 'onsite storage available', 'washer/dryer hookups in every unit and stackable washer/dryer in select units', 'close proximity to local shops', 'shower - secondary bath', 'gold', 'convenient location to apple; dell; hp and samsung', 'national relocation program', 'painted accent walls', 'wood-style flooring (carpet in bedroom)']
-    nlp = nlp_processor(word2vec_model,test_list)
-    nlp.main()
+    nlp = nlp_processor()
+    nlp.main(test_list)
--- preprocessing.py
+++ preprocessing.py
@@ -1,47 +1,60 @@
 from sklearn.preprocessing import LabelEncoder
-import ast
+from models.nlp_amenities import nlp_processor
 import pandas as pd
 import os
-import time
-import json  
-import ast
+import ast  # Used for converting string representation of lists back to lists
 
 class Preprocessing:
     def __init__(self, city, state):
+        # Set city and state
         self.city = city
         self.state = state
-        # Construct the file path for the JSONL (JSON Lines) file
+
+        # Construct file paths for unit and general information CSV files
         units_path = os.path.join("data", f"{city}_{state}", f"{city}_{state}_units.csv")
-        general_info = os.path.join("data",f"{city}_{state}", f"{city}_{state}_info.csv")
-        # Create a DataFrame from the data
-        units = pd.read_csv(units_path)
-        gen_info = pd.read_csv(general_info)  
-        self.main_data = main = pd.merge(gen_info, units, on='PropertyId', how='left').drop_duplicates()
+        general_info_path = os.path.join("data", f"{city}_{state}", f"{city}_{state}_info.csv")
+
+        # Load data into DataFrames
+        self.units = pd.read_csv(units_path)
+        self.gen_info = pd.read_csv(general_info_path)
+
+        # Initialize NLP processor
+        self.nlp_processor = nlp_processor()
         
     def clean_sq_foot_column(self):
-        self.main_data = self.main_data.dropna(subset=['SquareFootage','MaxRent'])
-        self.main_data['SquareFootage'] = self.main_data['SquareFootage'].str.replace(',', '').astype(int)
+        # Drop rows with missing values in 'SquareFootage' and 'MaxRent', and convert 'SquareFootage' to int
+        self.units.dropna(subset=['SquareFootage', 'MaxRent'], inplace=True)
+        self.units['SquareFootage'] = self.units['SquareFootage'].str.replace(',', '').astype(int)
         
     def encode_categories(self):
-        ''' represent categorical data such as neighborhoods into int'''
+        # Convert categorical data such as 'Neighborhood' into integers
         label_encoder = LabelEncoder()
-        self.main_data['Neighborhood_Label'] = label_encoder.fit_transform(self.main_data['Neighborhood'])
-        self.main_data['VerifiedListing'] = self.main_data['VerifiedListing'].apply(lambda x: 1 if x == 'Verified Listing' else 0)
+        self.gen_info['Neighborhood_Label'] = label_encoder.fit_transform(self.gen_info['Neighborhood'])
+        self.gen_info['VerifiedListing'] = self.gen_info['VerifiedListing'].apply(lambda x: 1 if x == 'Verified Listing' else 0)
 
-    def clean_amenities(self):
-        self.main_data['Amenities'] = self.main_data['Amenities'].apply(ast.literal_eval)
-        self.main_data['Amenities'] = self.main_data['Amenities'].apply(lambda amenities_list: [amenity.replace('*', '') for amenity in amenities_list])
-        unique_set = set(word.lower() for row in self.main_data['Amenities'] for word in row)
-        print(unique_set)
+    def process_amenities(self):
+        # Convert amenities from string representations to lists, clean up, and convert to vectors
+        self.gen_info['Amenities'] = self.gen_info['Amenities'].apply(ast.literal_eval)
+        self.gen_info['Amenities'] = self.gen_info['Amenities'].apply(
+            lambda amenities_list: [amenity.replace('*', '').lower() for amenity in amenities_list]
+        )
+        self.gen_info['Amenities_Vector'] = self.gen_info['Amenities'].apply(
+            lambda x: self.nlp_processor.convert_amenities_to_vector(x)
+        )
         
-
     def main(self):
+        # Process data
         self.clean_sq_foot_column()
         self.encode_categories()
-        self.clean_amenities()
-    
-        # pd.set_option('display.max_columns', None)
-        # print(self.main_data.head())
+        self.process_amenities()
+
+        # Merge DataFrames and prepare the final DataFrame for analysis
+        final_df = pd.merge(self.gen_info, self.units, on='PropertyId', how='left')
+        final_df = final_df[['PropertyId', 'ReviewScore', 'Neighborhood_Label', 'Amenities_Vector', 'Baths', 'Beds', 'MaxRent', 'SquareFootage']].dropna()
+
+        # Display the prepared DataFrame
+        pd.set_option('display.max_columns', None)
+        print(final_df.head(40))
 
 if __name__ == '__main__':
     city, state = 'austin', 'tx'
